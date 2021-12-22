2bf04e99c2e78d5eda29201586a8ab09785d5620
kalitakd <KalitaKD@yandex.ru>
2021-12-22 17:26:32 +0300

    Begin generator. Rename questions

--- meetup-telegram-bot/Data/MainKeyboard.cs
+++ meetup-telegram-bot/Data/MainKeyboard.cs
@@ -2,10 +2,10 @@
 {
     public static class MainKeyboard
     {
-        public const string FirstPresentationQuestion = "Задать вопрос по теме 1";
-        public const string SecondPresentationQuestion = "Задать вопрос по теме 2";
-        public const string ThirdPresentationQuestion = "Задать вопрос по теме 3";
-        public const string OutOfPresentationQuestion = "Задать вопрос не по темам";
+        public const string FirstPresentationQuestion = "Путь .NET-разработчика";
+        public const string SecondPresentationQuestion = "Работа с системой Git";
+        public const string ThirdPresentationQuestion = "In-live разработчика ASP.NET приложения";
+        public const string OutOfPresentationQuestion = "Вопрос не по теме";
         public const string LeaveFeedback = "Оставить отзыв";
     }
 }
---
+++ meetup-telegram-bot/Services/AuthorNameGenerator.cs
@@ -0,0 +1,41 @@
+﻿namespace meetup_telegram_bot.Services
+{
+    public static class AuthorNameGenerator
+    {
+        private static List<string> nouns = new List<string>() 
+        {
+            "Джун",
+            "Сеньор",
+            "Стажер",
+            "Баг",
+            "Новичок",
+            "Архитектор",
+            "Девопс",
+            "Энтузиаст",
+            "Тестировщик",
+            "Жавист",
+            "Сын маминой подруги"
+        };
+        private static List<string> adjectives = new List<string>() 
+        { 
+            "Прекрасный",
+            "Изнемогающий",
+            "Загадочный",
+            "Безумный",
+            "Жаждущий",
+            "Затаившийся",
+            "Богатый",
+            "Задумчивый",
+            "Радостный",
+            "Взволнованный",
+            "Мстительный",
+        };
+        public static string Generate()
+        {
+            Random rnd = new Random();
+            //var  = rnd.Next();
+            return "s";
+
+        }
+    }
+}
